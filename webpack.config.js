3351fb2cd8f7f2de38402043f97bc5e454315a0f
Tw93 <tw93@qq.com>
2019-11-05 21:10:35 +0800

    replace demo html

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: '[name].bundle.js',
-    path: resolve(__dirname, 'demo_build'),
+    path: resolve(__dirname, 'demo/dist'),
   },
   module: {
     rules: [
