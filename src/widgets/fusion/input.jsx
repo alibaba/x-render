7c9aa628d86bdd75a73e2f0ac377a340859ca924
Tw93 <tw93@qq.com>
2019-12-01 16:21:32 +0800

    feat: add preview image for fusion

--- src/widgets/fusion/input.jsx
+++ src/widgets/fusion/input.jsx
@@ -1,35 +1,25 @@
 import React from 'react';
-import { Input } from '@alifd/next';
-// import { Input, Icon, Balloon } from '@alifd/next';
+import { Input, Icon, Balloon } from '@alifd/next';
+import previewContent from '../../components/previewContent';
 
-// TODO: fusion图片预览效果不佳，暂不支持
-// const defaultImg =
-//   'https://img.alicdn.com/tfs/TB14tSiKhTpK1RjSZFKXXa2wXXa-354-330.png';
-// const previewNode = (format, value) => {
-//   if (format !== 'image') {
-//     return null;
-//   }
-//   const content = (
-//     <img
-//       src={value || defaultImg}
-//       alt="图片地址错误"
-//       className="fr-preview-image"
-//     />
-//   );
-//   return (
-//     <Balloon
-//       trigger={<Icon type="picture" />}
-//       className="fr-preview"
-//       align="tl"
-//     >
-//       {content}
-//     </Balloon>
-//   );
-// };
+const previewNode = (format, value) => {
+  if (format !== 'image') {
+    return null;
+  }
+  return (
+    <Balloon
+      trigger={<Icon type="picture" />}
+      className="fr-preview"
+      align="tl"
+    >
+      {previewContent(format, value)}
+    </Balloon>
+  );
+};
 export default function input(p) {
   const { options = {} } = p;
   const { addonBefore, addonAfter, ...rest } = options;
-  // const { format = 'text' } = p.schema;
+  const { format = 'text' } = p.schema;
   const handleChange = value => p.onChange(p.name, value);
   return (
     <Input
@@ -38,7 +28,7 @@ export default function input(p) {
       disabled={p.disabled}
       readOnly={p.readonly}
       addonTextBefore={addonBefore ? addonBefore : ''}
-      addonTextAfter={addonAfter ? addonAfter : ''}
+      addonTextAfter={addonAfter ? addonAfter : previewNode(format, p.value)}
       onChange={handleChange}
     />
   );
