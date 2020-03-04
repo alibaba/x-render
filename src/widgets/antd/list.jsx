57c6ff7bcbda6fcc8c89e57ba455316313693965
FateRiddle <fateriddle@gmail.com>
2020-03-04 15:16:42 +0800

    list icon compatible

--- src/widgets/antd/list.jsx
+++ src/widgets/antd/list.jsx
@@ -3,7 +3,31 @@
  * 数组组件
  */
 
+import React from 'react';
 import listHoc from '../../components/listHoc';
+import { Icon } from '@ant-design/compatible';
+import { PlusCircleOutlined, DeleteOutlined } from '@ant-design/icons';
+
 import { Button } from 'antd';
 
-export default listHoc(Button);
+function FrButton({ icon, children, ...rest }) {
+  let IconComponent;
+  switch (icon) {
+    case 'file-add':
+      IconComponent = <PlusCircleOutlined />;
+      break;
+    case 'delete':
+      IconComponent = <DeleteOutlined />;
+      break;
+    default:
+      IconComponent = typeof icon === 'string' ? <Icon type={icon} /> : icon;
+      break;
+  }
+  return (
+    <Button {...rest} icon={IconComponent}>
+      {children}
+    </Button>
+  );
+}
+
+export default listHoc(FrButton);
