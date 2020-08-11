174e5aed0ca6c439b0704e304b12ff7feef539b7
FateRiddle <fateriddle@gmail.com>
2020-08-11 16:40:29 +0800

    full screen demo

--- docs/playground/index.jsx
+++ docs/playground/index.jsx
@@ -61,7 +61,7 @@ class Root extends Component {
   render() {
     const { showDescIcon, readOnly, labelWidth } = this.state;
     return (
-      <div className="vh-100 overflow-auto flex flex-column relative">
+      <div className="fr-playground">
         {/* <GithubCorner
           href="https://github.com/alibaba/form-render"
           bannerColor="#F6C14F"
@@ -70,7 +70,70 @@ class Root extends Component {
         {/* <a href="/" className="f6 absolute top-1 right-1 z-999">
           回主页
         </a> */}
-        <Collapse defaultActiveKey={['1']} onChange={() => {}}>
+        <div className="w-100 flex setting-container">
+          <Radio.Group
+            name="schemaName"
+            defaultValue="simplest"
+            className="w-50 flex items-center flex-wrap z-999"
+            onChange={this.onSchemaChange}
+          >
+            <Radio value="simplest">最简样例</Radio>
+            <Radio value="basic">基础控件</Radio>
+            <Radio value="new-feature">新功能</Radio>
+            <Radio value="function">复杂联动</Radio>
+            <Radio value="input">个性输入框</Radio>
+            <Radio value="select">个性选择框</Radio>
+            <Radio value="date">日期</Radio>
+            <Radio value="demo">完整例子</Radio>
+          </Radio.Group>
+          <div className="w-50 flex items-center flex-wrap z-999">
+            <RadioGroup
+              options={themeList}
+              value={this.state.theme}
+              onChange={this.onThemeChange}
+            />
+            <Select
+              style={{ marginRight: 8 }}
+              onChange={this.onColumnNumberChange}
+              defaultValue="1"
+            >
+              <Option value="1">一行一列</Option>
+              <Option value="2">一行二列</Option>
+              <Option value="3">一行三列</Option>
+            </Select>
+            <Select
+              style={{ marginRight: 8 }}
+              onChange={this.onDisplayChange}
+              defaultValue="column"
+            >
+              <Option value="column">上下排列</Option>
+              <Option value="row">左右排列</Option>
+            </Select>
+            <Switch
+              style={{ marginRight: 8 }}
+              checkedChildren="关描述"
+              onChange={this.onShowDescChange}
+              unCheckedChildren="开描述"
+              checked={showDescIcon}
+            />
+            <Switch
+              style={{ marginRight: 8 }}
+              checkedChildren="编辑"
+              onChange={this.onReadOnlyChange}
+              unCheckedChildren="只读"
+              checked={readOnly}
+            />
+            <div style={{ width: 42 }}>标签：</div>
+            <Slider
+              style={{ width: 80 }}
+              max={200}
+              min={20}
+              value={labelWidth}
+              onChange={this.onLabelWidthChange}
+            />
+          </div>
+        </div>
+        {/* <Collapse defaultActiveKey={['1']} onChange={() => {}}>
           <Panel
             header={
               <div className="flex justify-between items-center">
@@ -79,71 +142,8 @@ class Root extends Component {
             }
             key="1"
           >
-            <div className="w-100 flex">
-              <Radio.Group
-                name="schemaName"
-                defaultValue="simplest"
-                className="w-50 flex items-center flex-wrap z-999"
-                onChange={this.onSchemaChange}
-              >
-                <Radio value="simplest">最简样例</Radio>
-                <Radio value="basic">基础控件</Radio>
-                <Radio value="new-feature">新功能</Radio>
-                <Radio value="function">复杂联动</Radio>
-                <Radio value="input">个性输入框</Radio>
-                <Radio value="select">个性选择框</Radio>
-                <Radio value="date">日期</Radio>
-                <Radio value="demo">完整例子</Radio>
-              </Radio.Group>
-              <div className="w-50 flex items-center flex-wrap z-999">
-                <RadioGroup
-                  options={themeList}
-                  value={this.state.theme}
-                  onChange={this.onThemeChange}
-                />
-                <Select
-                  style={{ marginRight: 8 }}
-                  onChange={this.onColumnNumberChange}
-                  defaultValue="1"
-                >
-                  <Option value="1">一行一列</Option>
-                  <Option value="2">一行二列</Option>
-                  <Option value="3">一行三列</Option>
-                </Select>
-                <Select
-                  style={{ marginRight: 8 }}
-                  onChange={this.onDisplayChange}
-                  defaultValue="column"
-                >
-                  <Option value="column">上下排列</Option>
-                  <Option value="row">左右排列</Option>
-                </Select>
-                <Switch
-                  style={{ marginRight: 8 }}
-                  checkedChildren="关描述"
-                  onChange={this.onShowDescChange}
-                  unCheckedChildren="开描述"
-                  checked={showDescIcon}
-                />
-                <Switch
-                  style={{ marginRight: 8 }}
-                  checkedChildren="编辑"
-                  onChange={this.onReadOnlyChange}
-                  unCheckedChildren="只读"
-                  checked={readOnly}
-                />
-                <div style={{ width: 42 }}>标签：</div>
-                <Slider
-                  style={{ width: 80 }}
-                  max={200}
-                  min={20}
-                  value={labelWidth}
-                  onChange={this.onLabelWidthChange}
-                />
-              </div>
-            </div>
-          </Panel>
-        </Collapse>
+            </Panel>
+        </Collapse> */}
         <Demo {...this.state} />
       </div>
     );
