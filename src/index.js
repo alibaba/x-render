1bdbe4105a3d25d0f4af431b17bcecb788084c1e
FateRiddle <fateriddle@gmail.com>
2020-09-11 11:58:06 +0800

    debounce validation & make renderField a component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useMemo } from 'react';
-import { usePrevious } from './hooks';
+import { usePrevious, useDebounce } from './hooks';
 import PropTypes from 'prop-types';
 import { isDeepEqual, combineSchema } from './base/utils';
 import { asField, DefaultFieldUI } from './base/asField';
@@ -9,7 +9,7 @@ import { getValidateList } from './base/validate';
 import './atom.css';
 import './index.css';
 
-function renderField(settings, fields, events) {
+function RenderField({ fields, onChange, ...settings }) {
   const { Field, props } = parse(settings, fields);
   if (!Field) {
     return null;
@@ -19,7 +19,7 @@ function renderField(settings, fields, events) {
       isRoot
       {...props}
       value={settings.data}
-      {...events}
+      onChange={onChange}
       formData={settings.formData}
     />
   );
@@ -34,8 +34,10 @@ const Wrapper = ({
   showValidate,
   ...rest
 }) => {
+  let _schema = {};
   const jsonSchema = schema || propsSchema; // 兼容schema字段和propsSchema字段
-  const _schema = combineSchema(jsonSchema, uiSchema);
+  // 将uiSchema和schema合并（推荐不写uiSchema）
+  _schema = combineSchema(jsonSchema, uiSchema);
 
   return (
     <FormRender
@@ -97,6 +99,15 @@ function FormRender({
     }
   }, [schema, formData]);
 
+  const debouncedValidate = useDebounce(onValidate);
+
+  // 用户输入都是调用这个函数
+  const handleChange = (key, val) => {
+    isUserInput.current = true;
+    onChange(val);
+    debouncedValidate(getValidateList(val, schema));
+  };
+
   const updateValidation = () => {
     onValidate(getValidateList(data, schema));
   };
@@ -119,38 +130,32 @@ function FormRender({
     generated[key] = gField;
   });
 
+  const settings = {
+    schema,
+    data,
+    name,
+    column,
+    showDescIcon,
+    showValidate,
+    displayType,
+    readOnly,
+    labelWidth,
+    useLogger,
+    formData: data,
+  };
+
+  const _fields = {
+    // 根据 Widget 生成的 Field
+    generated,
+    // 自定义的 Field
+    customized: fields,
+    // 字段 type 与 widgetName 的映射关系
+    mapping,
+  };
+
   return (
     <div className={`${className} fr-wrapper`}>
-      {renderField(
-        {
-          schema,
-          data,
-          name,
-          column,
-          showDescIcon,
-          showValidate,
-          displayType,
-          readOnly,
-          labelWidth,
-          useLogger,
-          formData: data,
-        },
-        {
-          // 根据 Widget 生成的 Field
-          generated,
-          // 自定义的 Field
-          customized: fields,
-          // 字段 type 与 widgetName 的映射关系
-          mapping,
-        },
-        {
-          onChange(key, val) {
-            isUserInput.current = true;
-            onChange(val);
-            onValidate(getValidateList(val, schema));
-          },
-        }
-      )}
+      <RenderField {...settings} fields={_fields} onChange={handleChange} />
     </div>
   );
 }
