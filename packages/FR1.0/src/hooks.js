cf4c7f9a336f3c281ddbfa49a37e6e86af6651dc
FateRiddle <fateriddle@gmail.com>
2021-04-06 09:50:23 +0800

    使用简单的useSet

--- packages/FR1.0/src/hooks.js
+++ packages/FR1.0/src/hooks.js
@@ -22,40 +22,42 @@ export const useStore = () => {
 
 // export default logger;
 
-// 类似于class component 的 setState
-export const useSet = initState => {
-  let show = useRef(true);
-  const [state, setState] = useReducer((state, newState) => {
-    let action = newState;
-    if (typeof newState === 'function') {
-      action = action(state);
-    }
-    if (newState.action && newState.payload) {
-      action = newState.payload;
-      if (typeof action === 'function') {
-        action = action(state);
-      }
-    }
-    const result = { ...state, ...action };
-    // if (newState.action !== 'no-log') {
-    // 解决会展示两遍的问题，TODO: 是否真的解决了？如果有不是每次重复显示两遍的咋办？
-    if (show.current === true) {
-      // console.group(newState.action || 'action'); // TODO: give it a name
-      // console.log('%cState:', 'color: #9E9E9E; font-weight: 700;', state);
-      // console.log('%cAction:', 'color: #00A7F7; font-weight: 700;', action);
-      // console.log('%cNext:', 'color: #47B04B; font-weight: 700;', result);
-      // console.groupEnd();
-      show.current = false;
-    } else {
-      show.current = true;
-    }
+export const useSet = x => useReducer((a, b) => ({ ...a, ...b }), x);
 
-    // } else {
-    // }
-    return result;
-  }, initState);
-  return [state, setState];
-};
+// 类似于class component 的 setState
+// export const useSet = initState => {
+//   let show = useRef(true);
+//   const [state, setState] = useReducer((state, newState) => {
+//     let action = newState;
+//     if (typeof newState === 'function') {
+//       action = action(state);
+//     }
+//     if (newState.action && newState.payload) {
+//       action = newState.payload;
+//       if (typeof action === 'function') {
+//         action = action(state);
+//       }
+//     }
+//     const result = { ...state, ...action };
+//     // if (newState.action !== 'no-log') {
+//     // 解决会展示两遍的问题，TODO: 是否真的解决了？如果有不是每次重复显示两遍的咋办？
+//     if (show.current === true) {
+//       console.group(newState.action || 'action'); // TODO: give it a name
+//       console.log('%cState:', 'color: #9E9E9E; font-weight: 700;', state);
+//       console.log('%cAction:', 'color: #00A7F7; font-weight: 700;', action);
+//       console.log('%cNext:', 'color: #47B04B; font-weight: 700;', result);
+//       console.groupEnd();
+//       show.current = false;
+//     } else {
+//       show.current = true;
+//     }
+
+//     // } else {
+//     // }
+//     return result;
+//   }, initState);
+//   return [state, setState];
+// };
 
 // start: true 开始、false 暂停
 export function useInterval(callback, delay, start) {
