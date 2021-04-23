0a20842bcabe455d742173efb5eadd23c16c29c7
FateRiddle <fateriddle@gmail.com>
2021-04-23 15:28:06 +0800

    html组件支持多选的展示

--- packages/form-render/src/widgets/antd/html.jsx
+++ packages/form-render/src/widgets/antd/html.jsx
@@ -5,8 +5,20 @@ export default function html({ value, schema }) {
   if (schema.type === 'boolean') {
     __html = value === true ? '✔' : '✘';
   } else if (Array.isArray(schema.enum) && Array.isArray(schema.enumNames)) {
-    const idx = schema.enum.indexOf(value);
-    __html = schema.enumNames[idx] || '-';
+    if (['string', 'number'].indexOf(typeof value) > -1) {
+      const idx = schema.enum.indexOf(value);
+      __html = schema.enumNames[idx] || '-';
+    } else if (Array.isArray(value)) {
+      let idxStr = '-';
+      value.forEach(v => {
+        const idx = schema.enum.indexOf(v);
+        const name = schema.enumNames[idx];
+        if (name) {
+          idxStr += ',' + name;
+        }
+      });
+      __html = idxStr.replace('-,', '');
+    }
   } else if (typeof value === 'number') {
     __html = String(value);
   } else if (typeof value === 'string') {
