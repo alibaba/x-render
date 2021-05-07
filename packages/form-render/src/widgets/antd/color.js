87eb19eaf1e117178a4f8be439dec369c86b4167
Dengju Deng <wj871287@gmail.com>
2021-05-07 11:52:03 +0800

    Update color.js

--- packages/form-render/src/widgets/antd/color.js
+++ packages/form-render/src/widgets/antd/color.js
@@ -137,7 +137,7 @@ export default function color(p) {
           type={format}
           animation="slide-up"
           color={p.value.slice(0, 7) || '#ffffff'}
-          alpha={getAlphaFromHex(color)}
+          alpha={getAlphaFromHex(p.value)}
           onChange={onPickerChange}
         />
       }
