aa8ac6339f959b36bca861699b41cd9c9c3f8943
FateRiddle <fateriddle@gmail.com>
2021-05-10 15:32:33 +0800

    测试例子更新

--- docs/form-render/.test/test-list.jsx
+++ docs/form-render/.test/test-list.jsx
@@ -5,6 +5,79 @@ import FormRender, { useForm } from 'form-render';
 const schema = {
   type: 'object',
   properties: {
+    listName: {
+      title: '对象数组',
+      description: '对象数组嵌套功能',
+      type: 'array',
+      min: 2,
+      max: 3,
+      props: {
+        hideTitle: true,
+        buttons: [
+          {
+            html: '测试1',
+            callback: 'cb1',
+          },
+          {
+            html: '<span style="color:red">测试2<span>',
+            callback: 'cb2',
+          },
+        ],
+      },
+      items: {
+        type: 'object',
+        properties: {
+          input1: {
+            title: '简单输入框',
+            type: 'string',
+            required: true,
+          },
+          select1: {
+            title: '单选',
+            type: 'string',
+            enum: ['a', 'b', 'c'],
+            enumNames: ['早', '中', '晚'],
+          },
+        },
+      },
+    },
+    listName1: {
+      title: '对象数组',
+      description: '对象数组嵌套功能',
+      type: 'array',
+      widget: 'list1',
+      min: 2,
+      max: 3,
+      props: {
+        hideTitle: true,
+        buttons: [
+          {
+            html: '测试1',
+            callback: 'cb1',
+          },
+          {
+            html: '<span style="color:red">测试2<span>',
+            callback: 'cb2',
+          },
+        ],
+      },
+      items: {
+        type: 'object',
+        properties: {
+          input1: {
+            title: '简单输入框',
+            type: 'string',
+            required: true,
+          },
+          select1: {
+            title: '单选',
+            type: 'string',
+            enum: ['a', 'b', 'c'],
+            enumNames: ['早', '中', '晚'],
+          },
+        },
+      },
+    },
     listName2: {
       title: '对象数组',
       description: '对象数组嵌套功能',
@@ -25,6 +98,18 @@ const schema = {
             enumNames: ['早', '中', '晚'],
           },
         },
+      },
+      props: {
+        buttons: [
+          {
+            html: '测试1',
+            callback: 'cb1',
+          },
+          {
+            html: '<span style="color:red">测试2<span>',
+            callback: 'cb2',
+          },
+        ],
       },
     },
     listName3: {
@@ -51,63 +136,29 @@ const schema = {
           },
         },
       },
-    },
-    listName1: {
-      title: '对象数组',
-      description: '对象数组嵌套功能',
-      type: 'array',
-      widget: 'list1',
       props: {
-        hideTitle: true,
-      },
-      items: {
-        type: 'object',
-        properties: {
-          input1: {
-            title: '简单输入框',
-            type: 'string',
-            required: true,
-          },
-          select1: {
-            title: '单选',
-            type: 'string',
-            enum: ['a', 'b', 'c'],
-            enumNames: ['早', '中', '晚'],
-          },
-        },
-      },
-    },
-    listName: {
-      title: '对象数组',
-      description: '对象数组嵌套功能',
-      type: 'array',
-      props: {
-        hideTitle: true,
         buttons: [
           {
-            html: 'sdf',
-            callback: 'someCallback',
+            html: '测试1',
+            callback: 'cb1',
+          },
+          {
+            html: '<span style="color:red">测试2<span>',
+            callback: 'cb2',
           },
         ],
       },
-      items: {
-        type: 'object',
-        properties: {
-          input1: {
-            title: '简单输入框',
-            type: 'string',
-            required: true,
-          },
-          select1: {
-            title: '单选',
-            type: 'string',
-            enum: ['a', 'b', 'c'],
-            enumNames: ['早', '中', '晚'],
-          },
-        },
-      },
     },
   },
+};
+
+window.cb1 = ({ value = [], onChange, schema }) => {
+  console.log(schema);
+  onChange([
+    ...value,
+    { input1: '123', select1: 'b' },
+    { input1: '122', select1: 'c' },
+  ]);
 };
 
 const Demo = () => {
@@ -122,7 +173,7 @@ const Demo = () => {
 
   return (
     <div>
-      <FormRender form={form} schema={schema} onFinish={onFinish} />
+      <FormRender debug form={form} schema={schema} onFinish={onFinish} />
       <Button type="primary" onClick={form.submit}>
         提交
       </Button>
--- docs/form-render/.test/tester.md
+++ docs/form-render/.test/tester.md
@@ -1,2 +1,2 @@
-<code src='./simple.jsx' />
+<code src='./simple2.jsx' />
 <!-- <code src='./test-list.jsx' /> -->
