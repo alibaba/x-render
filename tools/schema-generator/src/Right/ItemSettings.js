5b410c850426aeeb6a4df25ce4cf83eb5555f086
F-loat <chaimaoyuan@foxmail.com>
2021-05-13 14:27:31 +0800

    feat(generator): optimize itemSettings

--- tools/schema-generator/src/Right/ItemSettings.js
+++ tools/schema-generator/src/Right/ItemSettings.js
@@ -33,19 +33,16 @@ export default function ItemSettings() {
   };
 
   const getWidgetList = (settings, commonSettings) => {
-    let widgetList = [];
-    settings.forEach(setting => {
-      // TODO: 这里要判断一下否则会crash
-      const _widgets = setting.widgets;
-      const basicWidgets = _widgets
+    return settings.reduce((widgetList, setting) => {
+      if (!Array.isArray(setting.widgets)) return widgetList;
+      const basicWidgets = setting.widgets
         .map(item => ({
           ...item,
           widget: item.widget || item.schema.widget || getWidgetName(item.schema, defaultMapping),
           setting: { ...commonSettings, ...item.setting }
         }));
-      widgetList = [...widgetList, ...basicWidgets];
-    });
-    return widgetList;
+      return [...widgetList, ...basicWidgets];
+    }, []);
   };
 
   const onDataChange = (value) => {
@@ -62,38 +59,28 @@ export default function ItemSettings() {
   };
 
   useEffect(() => {
-    // 算 widgetList
-    const _settings = Array.isArray(settings)
-      ? [...settings, { widgets: [...elements, ...advancedElements, ...layouts] }] // TODO: 不是最优解
-      : defaultSettings;
-    const _commonSettings = isObject(commonSettings)
-      ? commonSettings
-      : defaultCommonSettings;
-    const widgetList = getWidgetList(_settings, _commonSettings);
-
     // setting 该显示什么的计算，要把选中组件的 schema 和它对应的 widgets 的整体 schema 进行拼接
-    let itemSelected;
-    let widgetName;
     try {
-      itemSelected = flatten[selected];
-      if (itemSelected) {
-        widgetName = getWidgetName(itemSelected.schema, defaultMapping);
-      }
-      if (widgetName) {
-        const element = widgetList.find(e => e.widget === widgetName) || {}; // 有可能会没有找到
-        const schemaNow = element.setting;
-        setSettingSchema({
-          type: 'object',
-          displayType: 'column',
-          showDescIcon: true,
-          properties: {
-            ...schemaNow,
-          },
-        });
-        setTimeout(() => {
-          form.setValues(itemSelected.schema);
-        }, 0);
-      }
+      const itemSelected = flatten[selected];
+      if (!itemSelected) return;
+      // 算 widgetList
+      const _settings = Array.isArray(settings)
+        ? [...settings, { widgets: [...elements, ...advancedElements, ...layouts] }] // TODO: 不是最优解
+        : defaultSettings;
+      const _commonSettings = isObject(commonSettings) ? commonSettings : defaultCommonSettings;
+      const widgetList = getWidgetList(_settings, _commonSettings);
+      const widgetName = getWidgetName(itemSelected.schema, defaultMapping);
+      const element = widgetList.find(e => e.widget === widgetName) || {}; // 有可能会没有找到
+      const schemaNow = element.setting;
+      setSettingSchema({
+        type: 'object',
+        displayType: 'column',
+        showDescIcon: true,
+        properties: {
+          ...schemaNow,
+        },
+      });
+      setTimeout(() => form.setValues(itemSelected.schema), 0);
     } catch (error) {
       console.log(error);
     }
