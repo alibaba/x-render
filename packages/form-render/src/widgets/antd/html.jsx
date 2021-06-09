3cc5de76e824ed8f034170231ff984aa5468e049
FateRiddle <fateriddle@gmail.com>
2021-06-09 11:35:49 +0800

    html支持range等的只读展示 #431

--- packages/form-render/src/widgets/antd/html.jsx
+++ packages/form-render/src/widgets/antd/html.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function html({ value, schema }) {
+export default function html({ value, schema = {} }) {
   let __html = '-';
   if (schema.type === 'boolean') {
     __html = value === true ? '✔' : '✘';
@@ -23,6 +23,15 @@ export default function html({ value, schema }) {
     __html = String(value);
   } else if (typeof value === 'string') {
     __html = value;
+  } else if (
+    schema.type === 'range' &&
+    Array.isArray(value) &&
+    value[0] &&
+    value[1]
+  ) {
+    __html = `${value[0]} - ${value[1]}`;
+  } else if (value && ['number', 'string'].indexOf(value) === -1) {
+    __html = JSON.stringify(value);
   }
 
   return (
