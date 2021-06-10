128e43a7fcb538ad94f43127f7298b1d8176fc95
Grart <grart_01@msn.com>
2021-06-10 12:10:35 +0800

    属性增加表单配置

--- tools/schema-generator/src/Provider.jsx
+++ tools/schema-generator/src/Provider.jsx
@@ -62,7 +62,7 @@ function Provider(props, ref) {
   const frwRef = ref || useRef();
   const [state, setState] = useSet({
     formData: {},
-    frProps: {}, // form-render 的全局 props 等
+    frProps: defaultValue?{displayType:defaultValue.displayType||'row',labelWidth:defaultValue.labelWidth||120,column:defaultValue.column||1}:{}, // form-render 的全局 props 等
     hovering: undefined, // 目前没有用到
     isNewVersion: true, // 用schema字段，还是用propsSchema字段，这是一个问题
     preview: false, // preview = false 是编辑模式
