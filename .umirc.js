154a3a5255d1a5dd5224f5f77f5a6a95472fcab5
FateRiddle <fateriddle@gmail.com>
2021-08-05 09:49:39 +0800

    chartRender entry return

--- .umirc.js
+++ .umirc.js
@@ -73,11 +73,11 @@ export default defineConfig({
           path:
             'https://github.com/alibaba/form-render/blob/master/packages/table-render/CHANGELOG.md',
         },
-        // {
-        //   title: 'ChartRender',
-        //   path:
-        //     'https://github.com/alibaba/form-render/blob/master/packages/chart-render/CHANGELOG.md',
-        // },
+        {
+          title: 'ChartRender',
+          path:
+            'https://github.com/alibaba/form-render/blob/master/packages/chart-render/CHANGELOG.md',
+        },
       ],
     },
     {
