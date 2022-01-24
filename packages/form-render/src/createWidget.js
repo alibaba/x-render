3c9f8a9e33e16d48052f11c0a35e899fa2072fc7
taobai <jiangzhifeng.jzf@alibaba-inc.com>
2022-01-24 20:46:05 +0800

    fix(createWidget): 修复设置trigger时导致无法onChange的问题

--- packages/form-render/src/createWidget.js
+++ packages/form-render/src/createWidget.js
@@ -49,6 +49,7 @@ export const transformProps = props => {
     onChange(newValue);
   };
   if (trigger && typeof trigger === 'string') {
+    controlProps.onChange = _onChange;
     controlProps[trigger] = _onChange;
   } else {
     controlProps.onChange = _onChange;
