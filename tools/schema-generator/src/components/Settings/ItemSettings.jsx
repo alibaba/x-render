c1d54f29f5f807cf64bbdaee64d3e5708bb3dea9
F-loat <chaimaoyuan@foxmail.com>
2022-02-11 12:07:58 +0800

    fix(generator): default number setting

--- tools/schema-generator/src/components/Settings/ItemSettings.jsx
+++ tools/schema-generator/src/components/Settings/ItemSettings.jsx
@@ -131,6 +131,7 @@ export default function ItemSettings({ widgets }) {
     <div style={{ paddingRight: 24 }}>
       <FormRender
         form={form}
+        removeHiddenData
         schema={settingSchema}
         widgets={{ ..._widgets, ...widgets }}
         watch={{
