957d4f57bb98912c599d71e652a475a1abb5f128
siyi98 <zhusijia1998@gmail.com>
2022-03-03 19:43:42 +0800

    chore(jest):save code

---
+++ packages/form-render/__tests__/core-utils.spec.ts
@@ -0,0 +1,24 @@
+import { flattenSchema } from '../src/form-render-core/src/utils';
+
+describe('Test FormRender Utils', () => {
+  it('Test flattenSchema', () => {
+    const schema = {
+      type: 'object',
+      properties: {
+        input1: {
+          title: '简单输入框',
+          type: 'string',
+          required: true,
+        },
+        select1: {
+          title: '单选',
+          type: 'string',
+          enum: ['a', 'b', 'c'],
+          enumNames: ['早', '中', '晚'],
+        },
+      },
+    };
+  });
+});
+
+console.log(JSON.stringify(flattenSchema(schema), null, 2));
