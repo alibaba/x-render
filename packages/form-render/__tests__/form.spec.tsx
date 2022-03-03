e41103703f6f9406e9e887d18f2ccb37e9819368
siyi98 <zhusijia1998@gmail.com>
2022-03-03 11:15:52 +0800

    chore(jest):save code

--- packages/form-render/__tests__/form.spec.tsx
+++ packages/form-render/__tests__/form.spec.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import FormRender, { useForm } from '../src/index';
 import { shallow, configure, mount } from 'enzyme';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 
 configure({ adapter: new Adapter() });
 
@@ -12,7 +14,7 @@ const schema = {
       title: '简单输入框',
       type: 'string',
       required: true,
-      default: 'form-render测试',
+      default: '简单输入框',
     },
     select1: {
       title: '单选',
@@ -26,14 +28,22 @@ const schema = {
 
 const Form = () => {
   const form = useForm();
-  const [state, setState] = useState();
+  const [state, setState] = useState({
+    input1: 'fr',
+    select1: 'd',
+  });
   const onFinish = (formData, errors) => {
     console.log('formData:', formData, 'errors', errors);
     setState(formData);
   };
+
   return (
     <div>
       <FormRender form={form} schema={schema as any} onFinish={onFinish} />
+      <div className="fr-value">
+        <div className="input">{state?.input1}</div>
+        <div className="select">{state?.select1}</div>
+      </div>
       <button id="submit" onClick={form.submit}>
         提交
       </button>
@@ -41,11 +51,30 @@ const Form = () => {
   );
 };
 
-test('Render FR Success', () => {
-  const wrapper = mount(<Form />);
-  expect(wrapper.find('button').length).toEqual(1);
-  const html = wrapper.html();
-  expect(html).toContain('fr-container');
-  expect(html).toContain('fr-field');
-  wrapper.unmount();
+function sleep(ms): Promise<never> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+describe('FormRender', () => {
+  it('📦  Render FR Success', async () => {
+    const wrapper = mount(<Form />);
+    act(() => {
+      wrapper.find('#submit').simulate('click');
+    });
+    await act(() => sleep(500));
+    expect(wrapper.find('.input').text()).toEqual('简单输入框');
+    expect(wrapper.find('.select').text()).toEqual('a');
+
+    act(() => {
+      wrapper.unmount();
+    });
+  });
+  // it('📦  FR Validate Success', async () => {
+  //   const { getByText } = render(<Form />);
+
+  //   fireEvent.change(getByText('fr'), {
+  //     target: { value: '' },
+  //   });
+  //   fireEvent.click(getByText('提交'));
+  // });
 });
