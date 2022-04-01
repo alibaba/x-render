1b1c3a85838c6d3177dc80468a8d0beb4d7489db
siyi98 <zhusijia1998@gmail.com>
2022-04-01 16:29:36 +0800

    feat(fr):upload支持自定义字段

--- packages/form-render/src/widgets/antd/upload.js
+++ packages/form-render/src/widgets/antd/upload.js
@@ -1,5 +1,6 @@
 import { UploadOutlined } from '@ant-design/icons';
 import { Button, message, Upload } from 'antd';
+import { get } from 'lodash-es'
 import React from 'react';
 
 export default function FrUpload({
@@ -8,6 +9,7 @@ export default function FrUpload({
   onChange,
   uploadProps,
   buttonProps,
+  schema
 }) {
   const props = {
     name: 'file',
@@ -16,7 +18,9 @@ export default function FrUpload({
     onChange(info) {
       if (info.file.status === 'done') {
         message.success(`${info.file.name} 上传成功`);
-        onChange(info.file.response.url);
+        const path = get(schema, 'props.path', '')
+        const url = path ? get(info.file.response, path) : info.file.response.url;
+        onChange(url);
       } else if (info.file.status === 'error') {
         message.error(`${info.file.name} 上传失败`);
       }
