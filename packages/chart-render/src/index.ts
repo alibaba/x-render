c350284b7fe8b226e71d3349870793bae8a3905f
mzl980425 <1198079622@qq.com>
2022-06-23 16:12:37 +0800

    feat(cr): 新增柱图和饼图

--- packages/chart-render/src/index.ts
+++ packages/chart-render/src/index.ts
@@ -2,3 +2,6 @@ export type { Schema } from 'form-render';
 export { ChartProvider, withChart } from './components/ChartProvider';
 export { default as Search } from './components/Search';
 export { useChart } from './utils/store';
+
+export { default as Column } from './widgets/Column';
+export { default as Pie } from './widgets/Pie';
