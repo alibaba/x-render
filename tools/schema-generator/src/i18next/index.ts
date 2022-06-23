d76c87fa494df80b8aa1b5d0e4588a0105a730aa
Will.xie <mylesshie@gmail.com>
2022-06-23 17:18:23 +0800

    feat: generator国际化，支持en/cn配置

---
+++ tools/schema-generator/src/i18next/index.ts
@@ -0,0 +1,16 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import resources from './locales/resources';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    debug: false,
+    resources: resources,
+    lng: "en",
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
+
+  export default i18n;
