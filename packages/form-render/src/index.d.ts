5d53bce55b1f1ab7833274b3db7c2702a7a1495c
siyi98 <zhusijia1998@gmail.com>
2022-09-20 11:43:39 +0800

    chore(workflow): delete mirror.yml

--- packages/form-render/src/index.d.ts
+++ packages/form-render/src/index.d.ts
@@ -27,7 +27,8 @@ export interface SchemaBase {
     | 'dateTime'
     | 'date'
     | 'time'
-    | 'upload';
+    | 'upload'
+    | (string & {});
   default: any;
   /** 是否必填，支持 `'{{ formData.xxx === "" }}'` 形式的表达式 */
   required: boolean | string;
