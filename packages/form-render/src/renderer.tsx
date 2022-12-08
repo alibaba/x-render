84be5d9e1901e3695225d886ccde2afa04df84f2
siyi98 <zhusijia1998@gmail.com>
2022-12-08 21:26:24 +0800

    chore: code style

--- packages/form-render/src/renderer.tsx
+++ packages/form-render/src/renderer.tsx
@@ -4,28 +4,31 @@ import FiledItem from './FiledItem';
 const FRender = (props: any): any => {
   const { schema, _namePath = [] } = props;
 
-  return Object.keys(schema?.properties || {}).map((path: any, index: number) => {
-    let children = null;
-    let namePath: any = [..._namePath, path];
+  return Object.keys(schema?.properties || {}).map(
+    (path: any, index: number) => {
+      let children = null;
+      let namePath: any = [..._namePath, path];
 
-    console.log(namePath, '-----------')
-    if (schema.properties[path]?.properties) {
-      children = FRender({ schema: schema.properties[path], _namePath: namePath });
-      namePath = null;
-    } else if (schema.properties[path]?.type) {
-      
+      console.log(namePath, '-----------');
+      if (schema.properties[path]?.properties) {
+        children = FRender({
+          schema: schema.properties[path],
+          _namePath: namePath,
+        });
+        namePath = null;
+      } else if (schema.properties[path]?.type) {
+      }
+
+      return (
+        <FiledItem
+          key={index}
+          schema={schema.properties[path]}
+          name={namePath}
+          children={children}
+        />
+      );
     }
-
-
-    return (
-      <FiledItem
-        key={index}
-        schema={schema.properties[path]}
-        name={namePath}
-        children={children}
-      />
-    )
-  });
-}
+  );
+};
 
 export default FRender;
