52b18aa9a0d4381dcc310b847f6648f66bcd9d0e
jaw52 <2135326728@qq.com>
2022-12-17 20:46:54 +0800

    build(单元测试): 优化配置，fix引入路径问题，支持覆盖率生成

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,13 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['@babel/plugin-transform-react-jsx'],
+      },
+    }) as any,
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
