94d6cacc0b7c6aef65607749914fdbdbbe99333c
zhanbo.lh <zhanbo.lh@alibaba-inc.com>
2023-01-12 13:57:07 +0800

    调试

--- packages/form-render/src/form-core/models/createFormStore.ts
+++ packages/form-render/src/form-core/models/createFormStore.ts
@@ -1,6 +1,6 @@
-import { createStore as create } from 'zustand';
+import { createStore as createx } from 'zustand';
+
 import { createContext, useContext } from 'react'
-import type { StoreApi } from 'zustand';
 import { _set, _get, _has, _cloneDeep } from '../../utils';
 import { flattenSchema as flatten } from '../../utils/flattenSchema';
 
@@ -14,8 +14,10 @@ type FormStore = {
   setSchema: (schema: any) => any;
 };
 
+console.log(createx, '-------createx')
+
 // 将 useStore 改为 createStore， 并把它改为 create 方法
-export const createStore = () => create<FormStore>((set: any, get: any) => ({
+export const createStore = () => createx<FormStore>((set: any, get: any) => ({
   isInit: false,
   schema: {},
   flattenSchema: {},
