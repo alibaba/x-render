d2d14c2cffdc2121a99479106cf6c981f7e85eaf
zhanbo.lh <zhanbo.lh@alibaba-inc.com>
2023-01-17 16:26:58 +0800

    feat: 增加只读模式图片组件显示图片

--- docs/form-render/demos/dependencies.jsx
+++ docs/form-render/demos/dependencies.jsx
@@ -1,6 +1,6 @@
 import { Button, Input, message } from 'antd';
 import FormRender, { useForm } from 'form-render';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const { TextArea } = Input;
 
@@ -10,33 +10,21 @@ const Demo = () => {
   const schema = {
     type: 'object',
     displayType: 'row',
-
     properties: {
-      // select1: {
-      //   title: '输入框',
-      //   type: 'string',
-      //   dependencies: ['useSelect'],
-      //   widget: 'MyTextEditor',
-      //   width: '60%',
-      // },
-      // useSelect: {
-      //   title: '输入框高度',
-      //   type: 'number',
-      //   width: '60%',
-      // },
-      image1: {
-        title: '图片展示',
+      select1: {
+        title: '输入框',
         type: 'string',
-        format: 'image'
+        dependencies: ['useSelect'],
+        widget: 'MyTextEditor',
+        width: '60%',
+      },
+      useSelect: {
+        title: '输入框高度',
+        type: 'number',
+        width: '60%',
       },
     },
   };
-
-  useEffect(() => {
-    form.setValues({
-      image1: 'https://img.alicdn.com/imgextra/i3/O1CN01J3IrQO1PrUx7YIpuM_!!6000000001894-2-tps-1136-674.png'
-    })
-  }, [])
 
   const onFinish = (data, errors) => {
     if (errors.length > 0) {
@@ -55,7 +43,6 @@ const Demo = () => {
         schema={schema}
         widgets={{ MyTextEditor }}
         onFinish={onFinish}
-        readOnly={true}
       />
       <Button type="primary" onClick={form.submit}>
         提交
