79b4ab57d042e860120b794ba4f9e5350e683c83
lhbxs <596850703@qq.com>
2023-02-20 20:18:02 +0800

    测试

--- api/proxy.js
+++ api/proxy.js
@@ -5,17 +5,17 @@ module.exports = (req, res) => {
     // 代理目标地址
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
     // target 替换为你跨域请求的服务器 如： http://baidu.com
-    if (req.url.startsWith('/backend')) {
+    if (req.url.startsWith('/docs__')) {
         target = 'https://fanyi-api.baidu.com'
     }
     // 创建代理对象并转发请求
     createProxyMiddleware({
         target,
         changeOrigin: true,
-        pathRewrite: {
-            // 通过路径重写，去除请求路径中的 `/backend`
-            // 例如 /backend/user/login 将被转发到 https://fanyi-api.baidu.com/user/login
-            '^/backend/': '/',
-        },
+        // pathRewrite: {
+        //     // 通过路径重写，去除请求路径中的 `/backend`
+        //     // 例如 /backend/user/login 将被转发到 https://fanyi-api.baidu.com/user/login
+        //     '^/backend/': '/',
+        // },
     })(req, res)
 }
