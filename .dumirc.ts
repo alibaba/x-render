2d51058a032640bff80ccbda6d5a7cfc840bc47a
lhbxs <596850703@qq.com>
2023-02-22 23:38:06 +0800

    打包测试

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,7 +4,6 @@ import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
 
 export default defineConfig({
   favicons: ['https://img.alicdn.com/tfs/TB17UtINiLaK1RjSZFxXXamPFXa-606-643.png'],
-  // autoAlias: false,
   outputPath: 'docs-dist',
   // locales: [{ id: 'zh-CN', name: '中文' }, { id: 'en-US', name: 'English' }],
   locales: [{ id: 'zh-CN', name: '中文' }],
@@ -48,7 +47,7 @@ export default defineConfig({
       'import',
       {
         libraryName: 'antd',
-        libraryDirectory: 'lib',
+        libraryDirectory: 'es',
         style: true,
       },
       'antd',
@@ -67,127 +66,13 @@ export default defineConfig({
     config.plugin('monaco-editor').use(MonacoWebpackPlugin);
   },
   plugins: [require.resolve('./scripts/dumi-plugin/redirect')],
-  // more config: https://d.umijs.org/config
   alias: { 
     'form-render':  path.resolve(__dirname, 'packages/form-render/src'),
     'table-render':  path.resolve(__dirname, 'packages/table-render/src'),
     'chart-render':  path.resolve(__dirname, 'packages/chart-render/src'),
     'fr-generator':  path.resolve(__dirname, 'tools/schema-generator/src')
   },
-  codeSplitting: { jsStrategy: 'granularChunks' },
-  analyze: {},
-  // headScripts: [
-  //   'https://unpkg.com/react@18.2.0/umd/react.production.min.js',
-  //   // 'https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js',
-  // ],
-  // externals: {
-  //   react: 'React',
-  //   // 'react-dom': 'ReactDOM',
-  //   // antd: 'antd',
-  //   // '@ant-design/icons': 'icons'
-  // },
+  codeSplitting: { jsStrategy: 'bigVendors' },
   //...(process.env.NODE_ENV === 'development' ? {} : { ssr: {} }),
 });
 
-// import { defineConfig } from 'dumi';
-// import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
-
-// export default defineConfig({
-//   title: 'XRender',
-//   favicon: 'https://img.alicdn.com/tfs/TB17UtINiLaK1RjSZFxXXamPFXa-606-643.png',
-//   logo: 'https://img.alicdn.com/tfs/TB17UtINiLaK1RjSZFxXXamPFXa-606-643.png',
-//   outputPath: 'docs-dist',
-//   hash: false,
-//   dynamicImport: {
-//     loading: '@/docs/Loading.js',
-//   },
-//   locales: [['zh-CN', '中文']],
-//   ignoreMomentLocale: false,
-//   navs: [
-//     {
-//       title: 'FormRender',
-//       path: '/form-render',
-//     },
-//     {
-//       title: 'TableRender',
-//       path: '/table-render',
-//     },
-//     {
-//       title: 'ChartRender',
-//       path: '/chart-render',
-//     },
-//     { title: '表单设计器', path: '/generator' },
-//     {
-//       title: 'Playground',
-//       path: '/playground',
-//     },
-//     // {
-//     //   title: '常见问题',
-//     //   path: '/faq',
-//     // },
-//     {
-//       title: '周边工具',
-//       path: '/tools',
-//       children: [
-//         { title: 'PropToSchema', path: '/tools/proptypes' },
-//         { title: 'VSCode 插件', path: '/tools/vscode' },
-//         {
-//           title: '旧版文档',
-//           path: 'https://x-components.gitee.io/form-render/',
-//         },
-//       ],
-//     },
-//     {
-//       title: '更新日志',
-//       children: [
-//         {
-//           title: 'FormRender',
-//           path: 'https://github.com/alibaba/x-render/blob/master/packages/form-render/CHANGELOG.md',
-//         },
-//         {
-//           title: 'TableRender',
-//           path: 'https://github.com/alibaba/x-render/blob/master/packages/table-render/CHANGELOG.md',
-//         },
-//         {
-//           title: 'ChartRender',
-//           path: 'https://github.com/alibaba/x-render/blob/master/packages/chart-render/CHANGELOG.md',
-//         },
-//       ],
-//     },
-
-//     { title: 'GitHub', path: 'https://github.com/alibaba/x-render' },
-//   ],
-//   mode: 'site',
-//   esbuild: {},
-//   // base: '/x-render/',
-//   // publicPath: '/x-render/',
-//   exportStatic: {},
-//   extraBabelPlugins: [
-//     [
-//       'import',
-//       {
-//         libraryName: 'antd',
-//         libraryDirectory: 'lib',
-//         style: true,
-//       },
-//       'antd',
-//     ],
-//     [
-//       'import',
-//       {
-//         libraryName: '@alifd/next',
-//         libraryDirectory: 'lib',
-//       },
-//       '@alifd/next',
-//     ],
-//   ],
-//   sitemap: {
-//     hostname: 'https://xrender.fun',
-//   },
-//   runtimePublicPath: true,
-//   chainWebpack(config, { webpack }) {
-//     config.plugin('monaco-editor').use(MonacoWebpackPlugin);
-//   },
-//   plugins: [require.resolve('./scripts/dumi-plugin/redirect')],
-//   // more config: https://d.umijs.org/config
-// });
--- packages/form-render/src/widgets/antd/datePicker.js
+++ packages/form-render/src/widgets/antd/datePicker.js
@@ -1,6 +1,6 @@
-import dayjsGenerateConfig from 'rc-picker/lib/generate/dayjs';
-import generatePicker from 'antd/lib/date-picker/generatePicker';
-import 'antd/lib/date-picker/style/index';
+import dayjsGenerateConfig from 'rc-picker/es/generate/dayjs';
+import generatePicker from 'antd/es/date-picker/generatePicker';
+import 'antd/es/date-picker/style/index';
 
 const DatePicker = generatePicker(dayjsGenerateConfig);
 
