82296cba5ec632babb84304171a8b5e4bce7b267
wb-hbc831417 <wb-hbc831417@alibaba-inc.com>
2023-03-27 16:07:37 +0800

    docs: 列设置文档示例调整

--- docs/table-render/demo/toolbar.tsx
+++ docs/table-render/demo/toolbar.tsx
@@ -4,21 +4,65 @@
  * background: 'rgb(245,245,245)'
  */
 import React, { useRef } from 'react';
-import TableRender from 'table-render';
-import { Button } from 'antd';
+import TableRender, { ProColumnsType } from 'table-render';
 import { schema } from '../static/search';
-import { columns, toolbarRender } from '../static/table';
-import { searchApi, searchApi2 } from '../static/request';
+import { searchApi } from '../static/request';
 
 const Demo = () => {
   const tableRef: any = useRef();
 
+  const columns: ProColumnsType<any> = [
+    {
+      title: '酒店名称',
+      dataIndex: 'title',
+      key: 'title', // 使用表格列设置功能，必须指定 key 的值
+      valueType: 'text',
+      width: '20%'
+    },
+    {
+      title: '酒店地址',
+      dataIndex: 'address',
+      key: 'address',
+      ellipsis: true,
+      copyable: true,
+      valueType: 'text',
+      width: '25%',
+    },
+    {
+      title: '酒店状态',
+      enum: {
+        open: '营业中',
+        closed: '已打烊',
+      },
+      dataIndex: 'state',
+      key: 'state',
+    },
+    {
+      title: '酒店星级',
+      dataIndex: 'labels',
+      key: 'labels',
+      width: 90,
+      valueType: 'tags'
+    },
+    {
+      title: '酒店GMV',
+      key: 'money',
+      sorter: true,
+      dataIndex: 'money',
+      valueType: 'money',
+    },
+    {
+      title: '成立时间',
+      key: 'created_at',
+      dataIndex: 'created_at',
+      valueType: 'date',
+    },
+  ];
+
   return (
     <TableRender
       ref={tableRef}
-      search={{
-        schema: schema
-      }}
+      search={{ schema }}
       scroll={{ x: 1500 }}
       request={searchApi}
       columns={columns}
--- docs/table-render/toolbar.md
+++ docs/table-render/toolbar.md
@@ -7,5 +7,11 @@ group:
 
 # 工具栏
 
+Table Render 内置工具栏，通过 `toolbarAction` 开启。默认四种功能，刷新表格、更改表格 `size`、全屏显示表格、表格列设置。
+
+:::warning
+使用表格列设置功能，必须为每个 `column` 指定 `key` 值
+:::
+
 <code src="./demo/toolbar.tsx"></code>
 
