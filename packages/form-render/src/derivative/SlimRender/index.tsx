a12cfbeadf44c5c1bf6f9952ec537c8c9be9a410
lhbxs <596850703@qq.com>
2023-04-04 22:44:32 +0800

    组件按需文档更新

--- packages/form-render/src/derivative/SlimRender/index.tsx
+++ packages/form-render/src/derivative/SlimRender/index.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import FormCore from '../../form-core';
 import withProvider from '../../withProvider';
 import Html from '../../widgets/field/readOnly';
-
 
 export default withProvider(FormCore, { Html });
