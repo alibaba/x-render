94a8e2df33bc7f6c457acec657558b94fbbb9121
lhbxs <596850703@qq.com>
2023-04-04 14:34:40 +0800

    更新资源

--- tools/schema-design/src/createIframe.ts
+++ tools/schema-design/src/createIframe.ts
@@ -7,39 +7,30 @@ const createIframeContent = () => {
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <title>XRender</title>
         <link rel="icon" href="https://img.alicdn.com/tfs/TB17UtINiLaK1RjSZFxXXamPFXa-606-643.png">
-        <link href="https://dev.g.alicdn.com/fone-lowcode/fr-generator/1.0.0/css/index.css" rel="stylesheet" />
-        <!-- 低代码引擎的页面主题样式，可以替换为 theme-lowcode-dark -->
         <link href="https://alifd.alicdn.com/npm/@alifd/theme-lowcode-light@0.2.1/variables.css" rel="stylesheet" />
         <link href="https://alifd.alicdn.com/npm/@alifd/theme-lowcode-light@0.2.1/dist/next.var.min.css" rel="stylesheet" />
-        <!-- 低代码引擎的页面框架样式 -->
-        <link rel="stylesheet" href="https://uipaas-assets.com/prod/npm/@alilc/lowcode-engine/1.1.3-beta.4/dist/css/engine-core.css" />
-        <!-- 低代码引擎官方扩展的样式 -->
+        <link rel="stylesheet" href="https://uipaas-assets.com/prod/npm/@alilc/lowcode-engine/1.1.5/dist/css/engine-core.css" />
         <link rel="stylesheet" href="https://uipaas-assets.com/prod/npm/@alilc/lowcode-engine-ext/1.0.5/dist/css/engine-ext.css" />
-        <link rel="stylesheet" href="https://dev.g.alicdn.com/fone-lowcode/fr-generator/1.0.0//index.css" />
+        <link rel="stylesheet" href="https://dev.g.alicdn.com/fone-lowcode/fr-generator/1.0.3/css/index.css" />
 
         <script>
           window.React = window.parent.React;
           window.ReactDOM = window.parent.ReactDOM;
         </script>
-
-        <!-- React 向下兼容，预防物料层的依赖 -->
+        
         <script src="https://g.alicdn.com/code/lib/prop-types/15.7.2/prop-types.js"></script>
         <script src="https://g.alicdn.com/platform/c/react15-polyfill/0.0.1/dist/index.js"></script>
-        <!-- lodash，低代码编辑器的依赖 -->
         <script src="https://g.alicdn.com/platform/c/lodash/4.6.1/lodash.min.js"></script>
-        <!-- 日期处理包，Fusion Next 的依赖 -->
         <script src="https://g.alicdn.com/mylib/moment/2.24.0/min/moment.min.js"></script>
-        <!-- Fusion Next 的主包，低代码编辑器的依赖 -->
         <script src="https://g.alicdn.com/code/lib/alifd__next/1.23.24/next.min.js"></script>
-        <!-- 低代码引擎的主包 -->
-        <script crossorigin="anonymous" src="https://uipaas-assets.com/prod/npm/@alilc/lowcode-engine/1.1.3-beta.4/dist/js/engine-core.js"></script>
-        <!-- 低代码引擎官方扩展的主包 -->
+        <script crossorigin="anonymous" src="https://uipaas-assets.com/prod/npm/@alilc/lowcode-engine/1.1.5/dist/js/engine-core.js"></script>
         <script crossorigin="anonymous" src="https://uipaas-assets.com/prod/npm/@alilc/lowcode-engine-ext/1.0.5/dist/js/engine-ext.js"></script>
+
       </head>
 
       <body>
         <div id="lce-container"></div>
-        <script type="text/javascript" src="https://dev.g.alicdn.com/fone-lowcode/fr-generator/1.0.0/js/index.js"></script>
+        <script type="text/javascript" src="https://dev.g.alicdn.com/fone-lowcode/fr-generator/1.0.3/js/index.js"></script>
       </body>
     </html>
   `;
