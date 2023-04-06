78bf55333671c40267ae991214022a6301de2a5e
lhbxs <596850703@qq.com>
2023-04-06 19:15:55 +0800

    fix: 修改文件目录

--- packages/form-render/src/derivative/SlimRender/index.tsx
+++ packages/form-render/src/derivative/SlimRender/index.tsx
@@ -1,5 +1,5 @@
 import FormCore from '../../form-core';
 import withProvider from '../../withProvider';
-import Html from '../../widgets/field/html';
+import Html from '../../widgets/fields/html';
 
 export default withProvider(FormCore, { Html });
