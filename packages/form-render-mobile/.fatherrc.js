791db43dd674578ae45800b722d758ada51f8ce6
lhbxs <596850703@qq.com>
2023-04-20 20:52:33 +0800

    修改开源群二维码

--- packages/form-render-mobile/.fatherrc.js
+++ packages/form-render-mobile/.fatherrc.js
@@ -7,29 +7,24 @@ export default {
     importLibToEs: true,
   },
   lessInBabelMode: true,
-  extraRollupPlugins: [
-    copy({
-      targets: [{ src: 'src/index.d.ts', dest: 'dist/' }],
-    }),
-  ],
   extraBabelPlugins: [
     [
       'import',
       {
         libraryName: 'antd-mobile',
-        libraryDirectory: 'es',
-        style: true,
+        libraryDirectory: 'es/components',
+        style: false,
       },
       'antd-mobile',
     ],
-    [
-      'import',
-      {
-        libraryName: '@ant-design/icons',
-        libraryDirectory: 'lib/icons',
-        camel2DashComponentName: false,
-      },
-      '@ant-design/icons',
-    ],
+    // [
+    //   'import',
+    //   {
+    //     libraryName: '@ant-design/icons',
+    //     libraryDirectory: 'lib/icons',
+    //     camel2DashComponentName: false,
+    //   },
+    //   '@ant-design/icons',
+    // ],
   ],
 };
