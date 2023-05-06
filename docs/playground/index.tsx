86a82090abab558658a138567648a215b19bc023
zhanbo.lh <zhanbo.lh@alibaba-inc.com>
2023-05-06 17:16:03 +0800

    优化 playground

--- docs/playground/index.tsx
+++ docs/playground/index.tsx
@@ -3,11 +3,11 @@ import FormRender, { useForm } from 'form-render';
 import debounce from 'lodash/debounce';
 import Editor from "@monaco-editor/react";
 import Controller from "./controller";
-import { Row, Col } from 'antd';
+import { Row, Col, Tabs } from 'antd';
 // @ts-ignore
 import { serializeToDraft, deserialize } from './serialize';
 import AsyncSelect from "./customized/AsyncSelect";
-import './index.css';
+import './index.less';
 
 const Playground = () => {
   const form = useForm();
@@ -17,6 +17,7 @@ const Playground = () => {
   const [readonly, setReadonly] = useState(false);
   const [labelWidth, setLabelWidth] = useState(100);
   const [lang, setLang] = useState('json');
+  const [formData, setFormData] = useState({});
 
   const onEditorChange = (val?: string) => {
     if (val) {
@@ -67,30 +68,66 @@ const Playground = () => {
   }
 
   const debounceEditorChange = useCallback(debounce(onEditorChange, 200), [])
-  const schema = useMemo(() => parseSchema(value), [value, lang]);
+  const schema = parseSchema(value);
+
+  const onValuesChange = () => {
+    setTimeout(() => {
+      form.submit();
+    })
+  }
 
   return (
     <div className="fr-playground">
         <Controller onChange={onControllerChange} />
         <Row gutter={24} style={{ flex: 1, overflow: 'hidden' }}>
           <Col span={12}>
-            <Editor
-              theme='vs-dark'
-              value={value}
-              language={lang}
-              options={{
-                lineNumbers: 'off',
-                fontFamily: "'JetBrains Mono', monospace",
-                fontSize: 14,
-                minimap: {
-                  enabled: false
+            <Tabs
+             
+              items={[
+                {
+                  label: 'Schema',
+                  key: 'schema',
+                  children: 
+                    <Editor
+                      theme='vs-dark'
+                      value={value}
+                      language={lang}
+                      options={{
+                        lineNumbers: 'off',
+                        fontFamily: "'JetBrains Mono', monospace",
+                        fontSize: 14,
+                        minimap: {
+                          enabled: false
+                        }
+                      }}
+                      onChange={debounceEditorChange}
+                      onMount={() => {
+                        const schema = require(`./json/simplest.json`);
+                        setValue(JSON.stringify(schema.schema, null, '\t'))
+                      }}
+                    />
+                  
+                },
+                {
+                  label: 'FormData',
+                  key: 'data',
+                  children: 
+                    <Editor
+                      theme='vs-dark'
+                      value={JSON.stringify(formData, null, '\t')}
+                      language={lang}
+                      options={{
+                        lineNumbers: 'off',
+                        readOnly: true,
+                        fontFamily: "'JetBrains Mono', monospace",
+                        fontSize: 14,
+                        minimap: {
+                          enabled: false
+                        }
+                      }}
+                    />
                 }
-              }}
-              onChange={debounceEditorChange}
-              onMount={() => {
-                const schema = require(`./json/simplest.json`);
-                setValue(JSON.stringify(schema.schema, null, '\t'))
-              }}
+              ]}
             />
           </Col>
           <Col span={12} style={{ overflowY: 'auto', overflowX: 'hidden', height: '100%',  background: '#fff', padding: '24px 24px 24px 24px', boxShadow: '0 2px 12px 0 rgba(0,0,0,.1)' }}>
@@ -103,6 +140,10 @@ const Playground = () => {
               labelWidth={labelWidth}
               widgets={{ asyncSelect: AsyncSelect }}
               footer={true}
+              watch={{ '#' : onValuesChange }}
+              onFinish={(values) => {
+                setFormData(values);
+              }}
             />
           </Col>
         </Row>
