1a34b3cf14b124447e4b5b9a8e521c3253e6f220
lhbxs <596850703@qq.com>
2023-06-01 12:52:01 +0800

    feat: 增加用例

--- packages/form-render/src/render-core/FieldItem/module.tsx
+++ packages/form-render/src/render-core/FieldItem/module.tsx
@@ -286,6 +286,8 @@ export const getFieldProps = (widgetName: string, schema: any, { widgets, method
     }
   }
 
+  console.log(fieldProps.options, 'fafadfaf', schema.title)
+
   // 以 props 结尾的属性，直接透传
   Object.keys(schema).forEach(key => {
     if (
