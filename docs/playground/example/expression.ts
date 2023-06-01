e99df5f885d71d3eeb5a6d5977a9323889d22969
lhbxs <596850703@qq.com>
2023-06-01 14:08:06 +0800

    feat: 增加用例

--- docs/playground/example/expression.ts
+++ docs/playground/example/expression.ts
@@ -1,33 +1,65 @@
 
-
 const case1 = {
-  "input1": {
-    "title": "输入框",
-    "type": "string",
-    "widget": "input"
-  },
-  "select1": {
-    "title": "下拉单选",
-    "type": "string",
-    "widget": "select",
-    "defaultValue": "{{ (formData.input1 === '111' && formData.select1 === 'b') ? undefined : formData.select1 }}",
-    "props": {
-      "options": [
-        {
-          "label": "早",
-          "value": "a",
-          "hidden": true
-        },
-        {
-          "label": "中",
-          "value": "b",
-          "hidden": "{{ formData.input1 === '111' }}"
-        },
-        {
-          "label": "晚",
-          "value": "c"
-        }
-      ]
+	"type": "object",
+  "properties": {
+    "input": {
+      "title": "输入框",
+      "type": "string",
+      "widget": "input",
+      "hidden": "{{ formData.hidden }}",
+      "disabled": "{{ formData.disabled }}",
+      "placeholder": "{{ formData.placeholder || '请输入' }}"
+    },
+    "placeholder": {
+      "title": "修改提示信息",
+      "type": "string",
+      "widget": "input"
+    },
+    "hidden": {
+      "title": "隐藏",
+      "type": "boolan",
+      "widget": "switch"
+    },
+    "disabled": {
+      "title": "禁用",
+      "type": "boolan",
+      "widget": "switch"
+    }
+  }
+}
+
+
+
+const case2 = {
+	"type": "object",
+  "properties": {
+    "input1": {
+      "title": "当输入框的值为 2 时，下拉单选第二个选项隐藏，如果已选中并清空选中值",
+      "type": "string",
+      "widget": "input"
+    },
+    "select1": {
+      "title": "下拉单选",
+      "type": "string",
+      "widget": "select",
+      "defaultValue": "{{ (formData.input1 === '2' && formData.select1 === 'b') ? undefined : formData.select1 }}",
+      "props": {
+        "options": [
+          {
+            "label": "早",
+            "value": "a"
+          },
+          {
+            "label": "中",
+            "value": "b",
+            "hidden": "{{ formData.input1 === '2' }}"
+          },
+          {
+            "label": "晚",
+            "value": "c"
+          }
+        ]
+      }
     }
   }
 }
