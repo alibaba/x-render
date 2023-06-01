e8d693bb30b956f711e7103a2d8963852b5cf67f
lhbxs <596850703@qq.com>
2023-06-01 17:00:00 +0800

    feat: diff 优化处理

--- packages/form-render/src/render-core/FieldItem/index.tsx
+++ packages/form-render/src/render-core/FieldItem/index.tsx
@@ -34,9 +34,7 @@ export default (props: any) => {
     >
       {(form: any) => {
         const formData = form.getFieldsValue(true);
-
-        console.log('我被更新啦', schema.title);
-
+        
         const formDependencies: any[] = [];
         const dependValues = (dependencies || []).map((depPath: string) => {
           const item:any[] = [];
