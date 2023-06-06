6869f38b40b8b5e92daa50fce3147f89bb2e0960
lhbxs <596850703@qq.com>
2023-06-06 14:05:06 +0800

    feat: 增加 widgetType 字段，控制渲染逻辑

--- docs/form-render/schema/simple.ts
+++ docs/form-render/schema/simple.ts
@@ -3,25 +3,7 @@ export default {
   properties: {
     input: {
       title: '输入框',
-      type: 'string',
-      widgetType: 'field',
-      properties: {
-        input: {
-          title: '输入框',
-          type: 'string'
-        },
-        select: {
-          title: '下拉框',
-          type: 'string',
-          props: {
-            options: [
-              { label: '早', value: 'a' },
-              { label: '中', value: 'b' },
-              { label: '晚', value: 'c' }
-            ]
-          }
-        }
-      }
+      type: 'string'
     },
     select: {
       title: '下拉框',
