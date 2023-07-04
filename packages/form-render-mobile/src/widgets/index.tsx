47efa1067ff0023e7c278cae637fbf64dee8e875
lhbxs <596850703@qq.com>
2023-07-04 10:17:40 +0800

    feat: 修复 fr-mobile radio 组件引入错误

--- packages/form-render-mobile/src/widgets/index.tsx
+++ packages/form-render-mobile/src/widgets/index.tsx
@@ -23,8 +23,8 @@ export const Rate = widgetHoc(AntdRate);
 export const Selector = widgetHoc(AntdSelector);
 export const Switch = widgetHoc(AntdSwitch);
 export const Stepper = widgetHoc(AntdStepper);
-export const Radio = widgetHoc(AntdRadio);
 
+export { default as Radio } from './Radio';
 export { default as DatePicker } from './DatePicker';
 export { default as Cascader } from './Cascader';
 export { default as Html } from './Html';
