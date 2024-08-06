56a66059cb964e58d84262c7e9417a8f556a6881
昔梦 <hemengying.hmy@alibaba-inc.com>
2024-08-06 20:57:21 +0800

    feat:1.drawerList新增drawerProps属性支持配置抽屉相关属性 2.新增hideColumnNestedObject支持设置drawerList表格内部复杂元素的折叠和隐藏

--- packages/form-render/src/render-core/index.tsx
+++ packages/form-render/src/render-core/index.tsx
@@ -1,37 +1,64 @@
+import { Collapse } from 'antd';
 import React from 'react';
+import sortProperties from '../models/sortProperties';
 import FieldItem from './FieldItem';
 import FieldList from './FieldList';
-import sortProperties from '../models/sortProperties';
 import './index.less';
 
 interface RenderCoreProps {
   schema: any;
   rootPath?: any[] | undefined;
   parentPath?: any[] | undefined;
-  [key: string]: any
-};
+  [key: string]: any;
+}
 
 interface RenderItemProps {
   schema: any;
   rootPath?: any[] | undefined;
   path?: any[] | undefined;
   key?: string | undefined;
-};
+  hideColumnNestedObject?: false | 'hide' | 'collapse';
+}
 
 const renderItem = (props: RenderItemProps) => {
-  let { schema, key, path, rootPath } = props;
+  let { schema, key, path, rootPath, hideColumnNestedObject } = props;
 
   // render List
   if (schema.type === 'array' && schema.items?.type === 'object') {
-    return (
-      <FieldList
-        key={key}
-        schema={schema}
-        path={path}
-        rootPath={rootPath}
-        renderCore={RenderCore}
-      />
-    );
+    if (hideColumnNestedObject === 'hide') {
+      return '-';
+    } else if (hideColumnNestedObject === 'collapse') {
+      return (
+        <Collapse
+          ghost
+          items={[
+            {
+              key: 'detail',
+              label: '查看详情',
+              children: (
+                <FieldList
+                  key={key}
+                  schema={schema}
+                  path={path}
+                  rootPath={rootPath}
+                  renderCore={RenderCore}
+                />
+              ),
+            },
+          ]}
+        />
+      );
+    } else {
+      return (
+        <FieldList
+          key={key}
+          schema={schema}
+          path={path}
+          rootPath={rootPath}
+          renderCore={RenderCore}
+        />
+      );
+    }
   }
 
   // render Objiect | field
@@ -39,8 +66,36 @@ const renderItem = (props: RenderItemProps) => {
 
   // has child schema
   if (schema?.properties && schema?.widgetType !== 'field') {
-    child = RenderCore({ schema, parentPath: path, rootPath })
+    child = RenderCore({ schema, parentPath: path, rootPath });
     // path = undefined;
+  }
+
+  if (schema.type === 'object' && hideColumnNestedObject === 'hide') {
+    return '-';
+  }
+
+  if (schema.type === 'object' && hideColumnNestedObject === 'collapse') {
+    return (
+      <Collapse
+        ghost
+        items={[
+          {
+            key: 'detail',
+            label: '查看详情',
+            children: (
+              <FieldItem
+                key={key}
+                schema={schema}
+                path={path}
+                rootPath={rootPath}
+                children={child}
+                renderCore={RenderCore}
+              />
+            ),
+          },
+        ]}
+      />
+    );
   }
 
   return (
@@ -49,29 +104,47 @@ const renderItem = (props: RenderItemProps) => {
       schema={schema}
       path={path}
       rootPath={rootPath}
-      children= {child}
+      children={child}
       renderCore={RenderCore}
     />
   );
-}
+};
 
-const RenderCore = (props: RenderCoreProps) : any => {
-  const { schema, parentPath = [], rootPath = [] } = props;
+const RenderCore = (props: RenderCoreProps): any => {
+  const {
+    schema,
+    parentPath = [],
+    rootPath = [],
+    hideColumnNestedObject = false,
+  } = props;
   if (!schema || Object.keys(schema).length === 0) {
     return null;
   }
- 
+
   // render List.item
   if (schema?.items) {
-    return renderItem({ schema: schema.items, path: parentPath, rootPath });
+    return renderItem({
+      schema: schema.items,
+      path: parentPath,
+      rootPath,
+      hideColumnNestedObject,
+    });
   }
 
   // render Objiect | field
-  return sortProperties(Object.entries(schema.properties || {})).map(([key, item]) => {
-    const path = [...parentPath, key];
-   
-    return renderItem({ schema: item, path, key, rootPath });
-  });
-}
+  return sortProperties(Object.entries(schema.properties || {})).map(
+    ([key, item]) => {
+      const path = [...parentPath, key];
+
+      return renderItem({
+        schema: item,
+        path,
+        key,
+        rootPath,
+        hideColumnNestedObject,
+      });
+    }
+  );
+};
 
 export default RenderCore;
