a93585518eec82bc8b840e609476584b740ad91a
jile1997 <1154416769@qq.com>
2025-01-08 11:24:36 +0800

    test: 测试

--- packages/x-flow/src/nodes/index.tsx
+++
@@ -1,7 +0,0 @@
-export { default as StartNode } from './node-start';
-export { default as EndNode } from './node-end';
-export { default as CommonNode } from './node-common';
-export { default as SwitchNode } from './node-switch';
-export { default as SwitchNodeSettingWidget } from './node-switch/setting';
-export { default as ParallelNode } from './node-parallel';
-export { default as ParallelNodeSettingWidget } from './node-parallel/setting';
--- packages/x-flow/src/utils/createIconFont.ts
+++
@@ -1,8 +0,0 @@
-import { createFromIconfontCN } from '@ant-design/icons';
-
-export default (url?: string) => {
-  return createFromIconfontCN({
-    // scriptUrl: url || '//at.alicdn.com/t/a/font_4201076_frx3c9x07if.js',
-    scriptUrl: url || '//at.alicdn.com/t/a/font_4069358_dd524fgnynb.js',
-  });
-};
