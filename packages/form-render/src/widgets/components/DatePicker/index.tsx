44c5a8c8dbbba8b506fd89dff25fbe96154d7058
昔梦 <hemengying.hmy@alibaba-inc.com>
2025-09-03 15:14:35 +0800

    fix：修复form-render中antd版本升级造成的 rc-picker@4.11.3版本不兼容问题造成的报错

--- packages/form-render/src/widgets/components/DatePicker/index.tsx
+++ packages/form-render/src/widgets/components/DatePicker/index.tsx
@@ -2,6 +2,14 @@ import dayjsGenerateConfig from 'rc-picker/es/generate/dayjs';
 import generatePicker from 'antd/es/date-picker/generatePicker';
 import 'antd/es/date-picker/style/index';
 
-const DatePicker: any = generatePicker(dayjsGenerateConfig);
+// 修复 dayjs 生成配置中缺失的毫秒相关方法
+const enhancedDayjsConfig = {
+  ...dayjsGenerateConfig,
+  // 添加缺失的毫秒相关方法
+  getMillisecond: (date: any) => date.millisecond(),
+  setMillisecond: (date: any, millisecond: number) => date.millisecond(millisecond),
+};
+
+const DatePicker: any = generatePicker(enhancedDayjsConfig);
 
 export default DatePicker;
