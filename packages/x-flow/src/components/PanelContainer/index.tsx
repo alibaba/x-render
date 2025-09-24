1bd90499204f804661612aaf5321e6e08cc2157f
昔梦 <hemengying.hmy@alibaba-inc.com>
2025-09-24 16:11:24 +0800

    fix：修复代码合并错误

--- packages/x-flow/src/components/PanelContainer/index.tsx
+++ packages/x-flow/src/components/PanelContainer/index.tsx
@@ -102,7 +102,7 @@ const Panel: FC<IPanelProps> = (props: IPanelProps) => {
       }
     });
     setNodes(newNodes, false);
-  }, 500);
+  }, 100);
 
   const handleTitleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
@@ -164,7 +164,7 @@ const Panel: FC<IPanelProps> = (props: IPanelProps) => {
     setTitleVal(data?.title || nodeSetting?.title);
     titleRef.current = data?.title || nodeSetting?.title;
     descRef.current = data?.desc;
-  }, [id,activeNode.data?.desc,activeNode.data?.title]);
+  }, [id, data?.desc, data?.title, nodeSetting?.title]);
 
   const Icon = useMemo(() => createIconFont(iconFontUrl), [iconFontUrl]);
 
